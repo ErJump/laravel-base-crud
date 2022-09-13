2f3085b3c0d5ad22b5fe858fabf8813301660cd2
ErJump <fedelijunior@gmail.com>
2022-09-13 14:08:02 +0200

    feat: :sparkles: Imports css and bootstrap

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Index')
+
+@section('content')
+    <h1>Comics</h1>
+    <ul>
+        @foreach ($comics as $comic)
+            <li>
+                {{$comic->title}}
+            </li>
+        @endforeach
+    </ul>
+@endsection
