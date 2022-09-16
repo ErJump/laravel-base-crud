1a2eac533667d4dc4d4acdb3922afbb7649988dd
ErJump <fedelijunior@gmail.com>
2022-09-16 13:50:04 +0200

    feat: :sparkles: Adds 404 blade

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', '404')
+
+@section('content')
+<div class="container-lg">
+    <img class='img-fluid' src="https://linkeasy.it/wp-content/uploads/errore-404.jpg" alt="404">
+</div>
+@endsection
